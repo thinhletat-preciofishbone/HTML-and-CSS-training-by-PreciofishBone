c05fca8a4a9d7708e651c45d0f1354edc81963d2
Thinh Le Tat <thinh.le.tat@preciofishbone.se>
2021-04-12 14:29:22 +0700

    create a constant for item icon classes

---
+++ src/scripts/constant/index.ts
@@ -0,0 +1,10 @@
+const itemIconClass = {
+  folder: 'bi bi-folder',
+  text: 'bi bi-journal-text',
+  word: 'bi bi-file-earmark-word-fill word-icon',
+  excel: 'bi bi-file-earmark-spreadsheet-fill excel-icon',
+  powerPoint: 'bi bi-file-earmark-ppt-fill power-point-icon',
+  oneNote: 'bi bi-journal-bookmark-fill one-note-icon',
+};
+
+export default itemIconClass;
